866c183405bc97bdeb9e9fb527eb72994bd562ee
Anthony Höhn <75019251+anthohn@users.noreply.github.com>
2023-08-17 15:55:58 +0200

    login form + remember me

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,10 +7,12 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['useEmail'], message: 'There is already an account with this useEmail')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
